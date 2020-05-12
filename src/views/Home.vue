bffb3d3234aee8623560826c83a8268f158e8ec3
CMonk <tianlang0704@gmail.com>
2020-05-12 12:26:52 +0800

    添加工作时间
    添加工作时间

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,27 +11,38 @@
                 v-progress-circular(
                   :size='280'
                   :width='40'
-                  :value='progress'
+                  :value='totalProgress'
                   :color='color'
                 )
-                  strong.headline {{ progress }}%
-                p.mt-3 {{ timeLeft }}
+                  v-progress-circular(
+                    :size='180'
+                    :width='20'
+                    :value='workHourProgressDisplay'
+                    :color='color'
+                    :rotate='workHourRotation'
+                  )
+                    strong.headline
+                      template(v-if='workHourProgressDisplay > 0') {{ workHourProgressDisplay }}%
+                      p {{ totalProgress }}%
+                      
+                p.mt-3 {{ totalTimeToTargetStr }}
                   v-btn(text, icon, color='grey', @click='dialog=true')
                     v-icon {{ mdiCog }}
+
             template(v-else)
               v-progress-linear(
                 :reverse='true'
-                :value="progress"
+                :value="totalProgress"
                 :color='color'
                 height="65")
               v-row
                 v-col
-                  strong {{ progress }}%
-                  p {{ timeLeft }}
-                  //- p now: {{ now.calendar() }}
-                  //- p 开始时间: {{ startDay.calendar() }}
-                  //- p 结束时间: {{ endDay.calendar() }}
-                  //- p 一天开始: {{ newDayBeginsDay.calendar() }}
+                  strong {{ totalProgress }}%
+                  p {{ totalTimeToTargetStr }}
+                  p now: {{ now.calendar() }}
+                  p 开始时间: {{ startRaw.calendar() }}
+                  p 结束时间: {{ endDayRaw.calendar() }}
+                  p 一天开始: {{ newDayBeginsRaw.calendar() }}
                 v-col
                   v-btn(text, icon, color='grey', @click='dialog=true').float-right
                     v-icon {{ mdiCog }}
@@ -53,7 +64,8 @@
                     v-divider.my-2
                     .subheader 高级选项
                     TimePicker(v-model='newDayBegins', label='新一天开始时间')
-                    //- TimePicker(v-model='offWork', label='下班时间')
+                    TimePicker(v-model='offWork', label='下班时间')
+                    v-switch(label='工作时间进度同步日进度', v-model='workProessSyncDay')                    
             v-card-actions
               v-spacer
               v-btn(color='primary', @click='save') 保存
@@ -71,13 +83,14 @@ export default {
   data(){
     return {
       dialog: false, //设置弹窗
-      start: localStorage.getItem('settings.start') || '9:00', //早上9点
+      start: localStorage.getItem('settings.startDay') || '9:00', //早上9点
       end: localStorage.getItem('settings.end') || '22:00', //晚上10点
       color: 'primary', // 进度条颜色
       circular: localStorage.getItem('settings.circular') == 'true', //圆环进度条
       now: dayjs(), //dayjs().set('hour', 1).set('minute',0).set('second', 45), 
-      newDayBegins: localStorage.getItem('settings.newDayBegins') || '6:00', //两天的分界线
-      offWork: localStorage.getItem('settings.offWork') || '20:00', //下班时间
+      newDayBegins: localStorage.getItem('settings.newDayBegins') || '0:00', //两天的分界线
+      offWork: localStorage.getItem('settings.offWork') || '19:00', //下班时间
+      workProessSyncDay: localStorage.getItem('settings.workProessSyncDay') || true, //上班进度是否同步一天进度显示
       mdiCog,
       mdiClock,
       mdiClose
@@ -87,40 +100,69 @@ export default {
     setInterval(()=>this.now = dayjs(),1000)
   },
   computed:{
-    newDayBeginsDay(){
-      let newDayBegins = this.newDayBegins.split(':')
-      newDayBegins = this.now.set('hour', newDayBegins[0]).set('minute', newDayBegins[1]).set('second', 0)
+    newDayBeginsRaw(){
+      let newDayBeginsStrArr = this.newDayBegins.split(':')
+      let newDayBegins = this.now.set('hour', newDayBeginsStrArr[0]).set('minute', newDayBeginsStrArr[1]).set('second', 0)
       return newDayBegins
     },
-    endDay(){
+    startRaw(){
+      let startStrArr = this.start.split(':').map(i=>parseInt(i))
+      let startDate = this.now.set('hour', startStrArr[0]).set('minute', startStrArr[1]).set('second', 0)
+      return startDate
+    },
+    endRaw(){
       let endStrArr = this.end.split(':').map(i=>parseInt(i))
       let endDate = this.now.set('hour', endStrArr[0]).set('minute', endStrArr[1]).set('second', 0)
-      if (!this.now.isBefore(endDate) && this.now.isBefore(this.newDayBeginsDay)) {
-        endDate = endDate.add(1, 'day')
-      }else if(!this.now.isBefore(endDate) && endDate.isBefore(this.startDay)){
-        endDate = endDate.add(1, 'day')
-      }
       return endDate
     },
-    startDay(){
-      let start = this.start.split(':').map(i=>parseInt(i))
-      start = this.now.set('hour', start[0]).set('minute', start[1]).set('second', 0)
-      return start
+    lastEndRaw(){
+      return this.endRaw.subtract(1, 'day')
     },
-
-    timeLeft(){
-      let timeLeft
-      let lastEndDay = this.endDay.subtract(1, "day")
-      if (!this.now.isBefore(lastEndDay) && this.now.isBefore(this.startDay)) {
-        timeLeft = this.now.to(lastEndDay)
-      } else {
-        timeLeft = this.now.to(this.endDay)
+    isEndBeforeStart(){
+      return this.endRaw.isBefore(this.startRaw)
+    },
+    trueEnd(){
+      if (this.isEndBeforeStart && !this.now.isBefore(this.endRaw)) {
+        return this.endRaw.add(1, 'day')
       }
-      return timeLeft.substr(0, timeLeft.length - 1)
+      return this.endRaw
     },
-    progress(){
+    trueLastEnd(){
+      return this.trueEnd.subtract(1, 'day')
+    },
+    timePastEnd(){
+      let lastEnd
+      if (this.now.isBefore(this.endRaw)) {
+        lastEnd = this.endRaw.subtract(1, 'day')
+      } else {
+        lastEnd = this.endRaw
+      }
+      return this.now.unix() - lastEnd.unix()
+    },
+    dayDuration(){
+      return this.trueEnd.unix() - this.startRaw.unix()
+    },
+    isNowSleepTime(){
+      let start
+      if (this.isEndBeforeStart) {
+        start = this.startRaw
+      } else {
+        start = this.startRaw.add(1, 'day')
+      }
+      return !this.now.isBefore(this.endRaw) && this.now.isBefore(start)
+    },
+    totalTimeToTargetStr(){
+      let timeLeft
+      if(this.isNowSleepTime) {
+        timeLeft = this.now.to(this.trueLastEnd)
+      } else {
+        timeLeft = this.now.to(this.endRaw)
+      }
+      return timeLeft.substr(0, timeLeft.length)
+    },
+    totalProgress(){
       // 更新颜色
-      if (!this.now.isBefore(this.endDay.subtract(1, "day")) && this.now.isBefore(this.newDayBeginsDay)) {
+      if (!this.now.isBefore(this.endRaw) && this.now.isBefore(this.newDayBeginsRaw)) {
         this.color = 'error'
       } else {
         this.color = 'primary'
@@ -128,25 +170,80 @@ export default {
 
       // 更新进度
       let progress
-      let lastEndDay = this.endDay.subtract(1, "day")
-      if (!this.now.isBefore(lastEndDay) && this.now.isBefore(this.startDay)) {
-        let lastEndDayUnix = lastEndDay.unix()
-        progress = Math.abs((this.now.unix() - lastEndDayUnix)/(lastEndDayUnix - this.endDay.unix()))
+      if (this.isNowSleepTime) {
+        progress = Math.abs(this.timePastEnd/(this.endRaw.unix() - this.startRaw.unix()))
       } else {
-        progress = Math.abs((this.now.unix() - this.endDay.unix())/(this.startDay.unix() - this.endDay.unix()))
+        let deltaEnd = this.now.unix() - this.trueEnd.unix()
+        let totalDuration = this.startRaw.unix() - this.trueEnd.unix()
+        progress = Math.abs(deltaEnd/totalDuration)
       }
-      progress = progress*100
+      progress = progress * 100
+      progress = progress.toFixed(3)
+      return progress
+    },
+    endWorkRaw(){
+      let offWork = this.offWork.split(':').map(i=>parseInt(i))
+      offWork = this.now.set('hour', offWork[0]).set('minute', offWork[1]).set('second', 0)
+      return offWork
+    },
+    isWorkEndBeforeStart(){
+      return this.endWorkRaw.isBefore(this.startRaw)
+    },
+    trueWorkEnd(){
+      if (this.isWorkEndBeforeStart && !this.now.isBefore(this.endWorkRaw)) {
+        return this.endWorkRaw.add(1, 'day')
+      }
+      return this.endWorkRaw
+    },
+    trueLastWorkEnd(){
+      return this.trueWorkEnd.subtract(1, 'day')
+    },
+    workTimeLeft(){
+      if (this.now.isBefore(this.trueLastWorkEnd)) {
+        return Math.max(this.trueLastWorkEnd.unix() - this.now.unix(), 0)
+      }else if (this.now.isBefore(this.startRaw)) {
+        return 0
+      } else {
+        return Math.max(this.trueWorkEnd.unix() - this.now.unix(), 0)
+      }
+    },
+    workHourDuration(){
+      let workHourDuration = Math.max(this.trueWorkEnd.unix() - this.startRaw.unix(), 0)
+      return workHourDuration
+    },
+    workHourPercentage(){
+      let dayDuration = Math.max(this.trueEnd.unix() - this.startRaw.unix(), 1)
+      return this.workHourDuration / dayDuration
+    },
+    workHourRotation(){
+      if (this.workProessSyncDay) {
+        return 360 * (1 - this.workHourPercentage)
+      } else {
+        return 0
+      }
+    },
+    workHourProgress() {
+      let progress = this.workTimeLeft / this.workHourDuration
+      progress = progress * 100
+      return progress
+    },
+    workHourProgressDisplay(){
+      let progress = this.workHourProgress
+      if(this.workProessSyncDay) {
+        progress = progress * this.workHourPercentage
+      }
       progress = progress.toFixed(3)
       return progress
     }
   },
   methods:{
     save(){
-      localStorage.setItem('settings.start', this.start)
+      localStorage.setItem('settings.startDay', this.start)
       localStorage.setItem('settings.end', this.end)
       localStorage.setItem('settings.dark', this.$vuetify.theme.dark)
       localStorage.setItem('settings.newDayBegins', this.newDayBegins)
       localStorage.setItem('settings.circular', this.circular)
+      localStorage.setItem('settings.workProessSyncDay', this.workProessSyncDay)
       this.dialog=false
     }
   },
