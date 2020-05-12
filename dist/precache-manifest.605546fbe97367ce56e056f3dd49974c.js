172d4b1824cfc012937cac1c03dcebbf1cb00b44
CMonk <tianlang0704@gmail.com>
2020-05-12 14:59:16 +0800

    更新预览

---
+++ dist/precache-manifest.605546fbe97367ce56e056f3dd49974c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "653ea8530ffe48190e75",
+    "url": "/css/app.211f4755.css"
+  },
+  {
+    "revision": "390667746b1a6aae105b",
+    "url": "/css/chunk-vendors.e82c53c6.css"
+  },
+  {
+    "revision": "07d1e22e8cddd99a53037dc510221b7b",
+    "url": "/img/logo.07d1e22e.svg"
+  },
+  {
+    "revision": "8ea3a0a96e32e41d767d9b905a2a0e49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "295b6d574e1da972f7e9",
+    "url": "/js/about.2c8a778e.js"
+  },
+  {
+    "revision": "653ea8530ffe48190e75",
+    "url": "/js/app.d73e7b71.js"
+  },
+  {
+    "revision": "390667746b1a6aae105b",
+    "url": "/js/chunk-vendors.1c4ece3d.js"
+  },
+  {
+    "revision": "294ee18ee4e99b47dab9a140422f5821",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
